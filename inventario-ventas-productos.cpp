e662c4e56e5b3b9a7233802454f1014886c21035
josue <josue.mlpro.2014@gmail.com>
2025-06-28 07:28:34 -0500

    funcionalidad añadida- salir del programa solo si el usuario lo desea

--- inventario-ventas-productos.cpp
+++ inventario-ventas-productos.cpp
@@ -2,4 +2,43 @@
 #include<string>
 
 using namespace std;
- 
+ //creamos una estructura producto
+struct producto{
+	
+	string nombre;
+	float precio;
+};
+
+//creamos otra estructura llamada venta
+struct venta{
+	
+	int idVenta;
+	string producto;
+	int cantidad;
+	float precioTotal;
+	
+};
+
+int main(){
+	string respuesta;
+	int hacer;
+	producto productos[10];
+	
+	venta ventas[10];
+	//condicion para saber si desea hacer algo mas
+	do{
+	//ingresamos que desea hacer el ususario
+	cout<<"menu_:"<<endl;
+	cout<<endl;
+	cout<<"registrar un nuevo producto (apretar 1)"<<endl;
+	
+	cin>>hacer;
+		
+	cout<<"deseas hacer algo mas? (si o no)"<<endl;
+	cin>> respuesta;
+		
+	}while(respuesta =="si");
+	
+	
+	return 0;
+}
