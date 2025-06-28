7bd626d20bdeca0e387d7119639dfa0eee72bde5
josue <josue.mlpro.2014@gmail.com>
2025-06-28 08:06:15 -0500

    avance 1-funcionalidad agregar producto

--- inventario-ventas-productos.cpp
+++ inventario-ventas-productos.cpp
@@ -22,21 +22,35 @@ struct venta{
 int main(){
 	string respuesta;
 	int hacer;
-	producto productos[10];
-	
-	venta ventas[10];
+	const int max=50;
+	int cant=0;
+	producto productos[max];
+	venta ventas[max];
 	//condicion para saber si desea hacer algo mas
 	do{
-	//ingresamos que desea hacer el ususario
 	cout<<"menu_:"<<endl;
 	cout<<endl;
 	cout<<"registrar un nuevo producto (apretar 1)"<<endl;
-	
+	//ingresamos que desea hacer el ususario
+	cout<<"que deseas hacer? "<<endl;
 	cin>>hacer;
+	//aplicar el cin.ingonre
+	cin.ignore();
+		//realizamos una condicion
+	if(hacer==1){
+		//agregamos un producto
+		cout<<"ingrese el nombre del producto: "<<endl;
+		getline(cin, productos[cant].nombre);
+		cout<<"ingrese el precio del producto: "<<endl;
+		cin>>productos[cant].precio;
 		
-	cout<<"deseas hacer algo mas? (si o no)"<<endl;
+	}
+	
+	
+	
+	cout<<"deseas hacer algo mas (si o no) " <<endl;	
+	 
 	cin>> respuesta;
-		
 	}while(respuesta =="si");
 	
 	
